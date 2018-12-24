75da71f588869adda0ed662ec8e0cbe401a24304
YOKOYAMA Go <38034251+Yokoyama-Go@users.noreply.github.com>
2018-12-24 15:50:49 +0900

    Update console_log.js

--- console_log.js
+++ console_log.js
@@ -1,1 +1,16 @@
-function
+/*
+You can shortcut "console.log ('');" to "c ('');".
+If you write  "import defaultExport from "https://github.com/Yokoyama-Go/js-shortcut/blob/master/console_log.js";" , you can get this.
+*/
+
+function c(m) {
+  m = '' + m '' // const m = text
+  if ("undefined" != m) { // Check it isn't empty or unknow
+    console.log(m);
+  }
+  else{
+  }
+  
+  
+//Thank you for read. 
+//By Yokoyama-Go(https://github.com/Yokoyama-Go/profile-Yokoyama_Go)
