745f6e1a76478955f304d239997a0056f08e0b0d
YOKOYAMA Go <38034251+Yokoyama-Go@users.noreply.github.com>
2018-12-24 14:39:06 +0900

    Update docment-write.js

--- docment-write.js
+++ docment-write.js
@@ -1,8 +1,15 @@
-//You can shortcut ```docment.write ('')``` to ```w ('')```.
-function w(m) { // Write function
-m = "" + m + ""; // const m = text 
-if ("undefined" != m) {  // Check it isn't empty or unknows
+/*
+You can shortcut "docment.write ('');" to "w ('');".
+If you write  "import defaultExport from "module-name";" , you can get this.
+*/
+
+function w(m) { 
+m = '' + m + '';  // const m = text 
+if ("undefined" != m) {  // Check it isn't empty or unknow
    document.write(m);
    }
-document.write("<BR>");
+   else{
+   }
+
+document.write("<br>");  
 }
