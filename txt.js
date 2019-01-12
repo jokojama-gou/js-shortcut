866e372954c6d030a68ff6f3e187d26880975b5a
YOKOYAMA Go <38034251+Yokoyama-Go@users.noreply.github.com>
2019-01-12 22:00:23 +0900

    Create txt.js

---
+++ txt.js
@@ -0,0 +1,16 @@
+/*
+You can shortcut "console.log ('');" to "c ('');".
+If you write  "import defaultExport from "https://github.com/Yokoyama-Go/js-shortcut/blob/master/console_log.js";" , you can get this.
+*/
+
+
+var fs = WScript.CreateObject("Scripting.FileSystemObject");
+
+
+
+function w(writetxtfilename) {
+  let writetxtfilename = " " + newtxtfilename + " " 
+  if  ("undifined" != writetxtfilename ){
+  file.write (writetextfilename)
+  }else{};
+}
