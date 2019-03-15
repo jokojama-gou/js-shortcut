59c7f769c9dd9a20c2c822f274a7a455adae5e7f
Og-Amayokoy <38034251+Og-Amayokoy@users.noreply.github.com>
2019-03-15 18:23:14 +0900

    Update docment_write.js

--- docment_write.js
+++ docment_write.js
@@ -1,6 +1,6 @@
 /*
 You can shortcut "docment.write ('');" to "w ('');".
-If you write  "import defaultExport from "https://github.com/Yokoyama-Go/js-shortcut/blob/master/docment-write.js";" , you can get this.
+If you write  "import defaultExport from "https://github.com/Og-Amayokoy/js-shortcut/blob/master/docment-write.js";" , you can get this.
 */
 
 function w(m) { 
